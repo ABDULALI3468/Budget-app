cf064742f2b3eb833002d63031b06a01bc6e8eec
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-10-15 11:20:56 +0500

    Upgraded End of Lines for Heroku.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
@@ -37,6 +39,7 @@ gem 'stimulus-rails'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem 'jbuilder'
+
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
---
+++ a.rb
@@ -0,0 +1,83 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.1.2'
+
+# Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
+gem 'rails', '~> 7.0.4'
+
+# Linters
+gem 'rubocop', '>= 1.0', '< 2.0'
+
+# Using Devise gem for Authentication
+gem 'devise'
+
+# font-awesome gem for icons
+# gem 'font-awesome-rails'
+gem 'sassc-rails'
+gem 'sass-rails'
+
+# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
+gem 'sprockets-rails'
+
+# Use sqlite3 as the database for Active Record
+gem 'pg', '~> 1.1'
+
+# Use the Puma web server [https://github.com/puma/puma]
+gem 'puma', '~> 5.0'
+
+# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
+gem 'importmap-rails'
+
+# Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
+gem 'turbo-rails'
+
+# Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
+gem 'stimulus-rails'
+
+# Build JSON APIs with ease [https://github.com/rails/jbuilder]
+gem 'jbuilder'
+
+# Use Redis adapter to run Action Cable in production
+# gem "redis", "~> 4.0"
+
+# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
+# gem "kredis"
+
+# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
+# gem "bcrypt", "~> 3.1.7"
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+
+# Reduces boot times through caching; required in config/boot.rb
+gem 'bootsnap', require: false
+
+gem 'cancancan'
+gem 'ffi'
+
+gem 'dotenv-rails'
+gem 'validate_url'
+
+gem 'faraday'
+# gem 'httparty'
+
+group :development, :test do
+  gem 'bullet'
+  gem 'capybara'
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'webdrivers', '~> 5.0'
+end
+
+group :development do
+  # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem 'letter_opener'
+  gem 'rails-controller-testing'
+  gem 'web-console'
+end
+
+group :test do
+  gem 'simplecov', require: false
+end
